a3306d75ea1835b2c13cdd2b94166dcc6e115cb4
unname <lwer210@naver.com>
2024-12-13 00:32:33 +0900

    feature: EnableJpaAuditing, EntityScan, EnableJpaRepositories 어노테이션 추가

--- src/main/java/com/example/oqp/OqpApplication.java
+++ src/main/java/com/example/oqp/OqpApplication.java
@@ -2,8 +2,14 @@ package com.example.oqp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.oqp.db.repository")
+@EntityScan(basePackages = "com.example.oqp.db.entity")
+@EnableJpaAuditing
 public class OqpApplication {
 
 	public static void main(String[] args) {
