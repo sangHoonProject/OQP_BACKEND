00557ca61435eeee0c02c963ffa704e9c9cef83a
unname <lwer210@naver.com>
2024-12-13 00:35:31 +0900

    feature: Login 비즈니스 로직 refresh 테이블 저장 로직 추가

--- src/main/java/com/example/oqp/domain/auth/service/AuthRestService.java
+++ src/main/java/com/example/oqp/domain/auth/service/AuthRestService.java
@@ -1,14 +1,18 @@
 package com.example.oqp.domain.auth.service;
 
 import com.example.oqp.common.enums.Role;
+import com.example.oqp.common.enums.UseYn;
 import com.example.oqp.common.error.CustomException;
 import com.example.oqp.common.error.ErrorCode;
 import com.example.oqp.common.jwt.JwtTokenResponse;
 import com.example.oqp.common.jwt.JwtUtil;
+import com.example.oqp.db.entity.JwtRefresh;
 import com.example.oqp.db.entity.UserInfo;
+import com.example.oqp.db.repository.JwtRefreshRepository;
 import com.example.oqp.db.repository.UserInfoRepository;
 import com.example.oqp.domain.auth.restcontroller.request.LoginRequest;
 import com.example.oqp.domain.auth.restcontroller.request.RegisterRequest;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,8 +20,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,10 +36,11 @@ public class AuthRestService {
     private final UserInfoRepository userInfoRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final JwtRefreshRepository jwtRefreshRepository;
     private final JwtUtil jwtUtil;
 
+    @Transactional
     public UserInfo register(RegisterRequest registerRequest) {
-
         if(userInfoRepository.existsByEmail(registerRequest.getEmail())) {
             throw new CustomException(ErrorCode.ALREADY_USED_EMAIL);
         }
@@ -45,21 +55,63 @@ public class AuthRestService {
                 .password(passwordEncoder.encode(registerRequest.getPassword()))
                 .nickname(registerRequest.getNickname())
                 .role(Role.ROLE_USER)
-                .regDt(LocalDateTime.now())
+                .totalHeartCount(0)
                 .build();
     }
 
+    @Transactional
     public JwtTokenResponse login(LoginRequest loginRequest) {
-
         try{
             UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
 
             Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
-            return jwtUtil.generateToken(authentication);
+            JwtTokenResponse response = jwtUtil.generateToken(authentication);
+
+            String refresh = response.getRefreshToken();
+
+            Claims claims = jwtUtil.parseClaims(refresh);
+
+            Date expiration = claims.getExpiration();
+
+            LocalDateTime refreshLocalDate = expiration.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+            UserInfo userInfo = userInfoRepository.findByEmail(loginRequest.getEmail());
+
+            List<JwtRefresh> userRefreshList = jwtRefreshRepository.findByUserId(userInfo.getId());
+
+            JwtRefresh jwtRefresh = toJwtRefresh(refresh, refreshLocalDate, userInfo);
+
+            jwtRefreshRepository.save(jwtRefresh);
+
+            validateUserRefreshList(userRefreshList);
+
+            jwtRefreshRepository.saveAll(userRefreshList);
+
+            return response;
+
         }catch (Exception e){
             throw new CustomException(ErrorCode.LOGIN_FAILED);
         }
 
+    }
+
+    private JwtRefresh toJwtRefresh(String refresh, LocalDateTime refreshLocalDate, UserInfo userInfo) {
+        return JwtRefresh.builder()
+                .useYn(UseYn.Y)
+                .token(refresh)
+                .expiredAt(refreshLocalDate)
+                .userId(userInfo.getId())
+                .build();
+    }
+
+    private void validateUserRefreshList(List<JwtRefresh> userRefreshList) {
+        if(!userRefreshList.isEmpty()){
+            userRefreshList.forEach(user -> {
+                if(UseYn.Y.equals(user.getUseYn())){
+                    user.setUseYn(UseYn.N);
+                }
+            });
+        }
     }
 }
