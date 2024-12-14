511a15a11af2f1103c034d5bdedc7184d39f6496
unname <lwer210@naver.com>
2024-12-14 15:57:18 +0900

    feature: BaseEntity 상속 제거

--- src/main/java/com/example/oqp/db/entity/JwtRefresh.java
+++ src/main/java/com/example/oqp/db/entity/JwtRefresh.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @ToString
-public class JwtRefresh extends BaseEntity {
+public class JwtRefresh {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
