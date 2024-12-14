0a3b4c434fb4415592dada5478bef8dd13c35a9d
unname <lwer210@naver.com>
2024-12-14 11:12:09 +0900

    Initial commit

--- src/main/java/com/example/oqp/OqpApplication.java
+++ src/main/java/com/example/oqp/OqpApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.oqp.db.repository")
