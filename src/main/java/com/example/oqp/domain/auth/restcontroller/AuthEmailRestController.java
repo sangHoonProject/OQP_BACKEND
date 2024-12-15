41b2eb446183aba66665c53ebc4d1a4e940cea21
unname <lwer210@naver.com>
2024-12-15 22:29:20 +0900

    feature: 비밀번호 재설정 인증 코드 발송 API 추가

--- src/main/java/com/example/oqp/domain/auth/restcontroller/AuthEmailRestController.java
+++ src/main/java/com/example/oqp/domain/auth/restcontroller/AuthEmailRestController.java
@@ -2,6 +2,7 @@ package com.example.oqp.domain.auth.restcontroller;
 
 import com.example.oqp.common.error.ErrorResponse;
 import com.example.oqp.db.entity.MailCode;
+import com.example.oqp.db.entity.PasswordAuthCode;
 import com.example.oqp.domain.auth.restcontroller.request.EmailSendRequest;
 import com.example.oqp.domain.auth.restcontroller.request.EmailVerifyRequest;
 import com.example.oqp.domain.auth.restcontroller.response.EmailVerifyResponse;
@@ -12,6 +13,8 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/mail")
 @RequiredArgsConstructor
+@Tag(name = "메일 발송 API Controller")
 public class AuthEmailRestController {
 
     private final AuthEmailRestService authEmailRestService;
@@ -52,5 +56,29 @@ public class AuthEmailRestController {
             @RequestBody EmailSendRequest request
     ) {
         return ResponseEntity.ok(authEmailRestService.send(request));
+    }
+
+    @SneakyThrows
+    @Operation(summary = "비밀번호 재설정 메일 발송 API", description = "비밀번호 분실 또는 재설정 필요 시 호출하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "발송 성공", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PasswordAuthCode.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "비밀번호 재설정할 사용자를 찾지 못했을 경우 반환", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "이메일 발송 실패시 반환", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+            })
+    })
+    @PostMapping("/password-code")
+    public ResponseEntity<PasswordAuthCode> sendAuthCode(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    description = "비밀번호 재설정 인증 코드 발급 요청 객체"
+            )
+            @RequestBody EmailSendRequest request
+    ){
+        return ResponseEntity.ok(authEmailRestService.sendAuthCode(request));
     }
 }
