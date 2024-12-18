76f7289f2336f1fe5d54168cadec3929e76b20c6
unname <lwer210@naver.com>
2024-12-18 22:09:33 +0900

    feature: cors 설정

--- src/main/java/com/example/oqp/common/config/CorsConfig.java
+++ src/main/java/com/example/oqp/common/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowCredentials(true)
+                .allowCredentials(false)
                 .allowedHeaders("*")
                 .allowedMethods("*");
     }
