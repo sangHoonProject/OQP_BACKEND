4bd0f2f443f74888f705ae91db203c056b8d96a3
unname <lwer210@naver.com>
2024-12-18 22:58:43 +0900

    feature: 비밀번호 재설정 요청 객체 추가

---
+++ src/main/java/com/example/oqp/domain/auth/restcontroller/request/PasswordUpdateRequest.java
@@ -0,0 +1,20 @@
+package com.example.oqp.domain.auth.restcontroller.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PasswordUpdateRequest {
+
+    private String email;
+
+    private String oldPassword;
+
+    private String newPassword;
+}
