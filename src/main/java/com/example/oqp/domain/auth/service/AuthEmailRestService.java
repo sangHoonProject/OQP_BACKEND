14976ba37beae99da3437687ce278060d23972ce
unname <lwer210@naver.com>
2024-12-18 22:49:47 +0900

    feature: 임시 비밀번호 발급 API 비즈니스 로직 추가

--- src/main/java/com/example/oqp/domain/auth/service/AuthEmailRestService.java
+++ src/main/java/com/example/oqp/domain/auth/service/AuthEmailRestService.java
@@ -4,14 +4,11 @@ import com.example.oqp.common.enums.UseYn;
 import com.example.oqp.common.error.CustomException;
 import com.example.oqp.common.error.ErrorCode;
 import com.example.oqp.db.entity.MailCode;
-import com.example.oqp.db.entity.PasswordAuthCode;
 import com.example.oqp.db.entity.UserInfo;
 import com.example.oqp.db.repository.MailCodeRepository;
-import com.example.oqp.db.repository.PasswordAuthCodeRepository;
 import com.example.oqp.db.repository.UserInfoRepository;
 import com.example.oqp.domain.auth.restcontroller.request.EmailSendRequest;
-import com.example.oqp.domain.auth.restcontroller.request.EmailVerifyRequest;
-import com.example.oqp.domain.auth.restcontroller.response.EmailVerifyResponse;
+import com.example.oqp.domain.auth.restcontroller.request.PasswordResetRequest;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -19,10 +16,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,8 +28,8 @@ public class AuthEmailRestService {
 
     private final UserInfoRepository userInfoRepository;
     private final MailCodeRepository mailCodeRepository;
-    private final PasswordAuthCodeRepository passwordAuthCodeRepository;
     private final JavaMailSender javaMailSender;
+    private final PasswordEncoder passwordEncoder;
 
     public MailCode send(EmailSendRequest request) throws MessagingException {
 
@@ -94,16 +91,7 @@ public class AuthEmailRestService {
 
         helper.setSubject(subject);
 
-        String content = getMailContent(secureCode);
-
-        helper.setText(content, true);
-
-        return mimeMessage;
-    }
-
-    private String getMailContent(String secureCode) {
-
-        return "<html>" +
+        String content = "<html>" +
                 "<body style='font-family: Arial, sans-serif; background-color: #f4f7fa; margin: 0; padding: 0;'>" +
                 "<div style='width: 100%; padding: 40px 20px; text-align: center; background-color: #ffffff; border-radius: 15px; box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1); max-width: 600px; margin: 50px auto;'>" +
 
@@ -125,74 +113,65 @@ public class AuthEmailRestService {
                 "</div>" +
                 "</body>" +
                 "</html>";
+
+        helper.setText(content, true);
+
+        return mimeMessage;
     }
 
-    public PasswordAuthCode sendAuthCode(EmailSendRequest request) throws MessagingException {
-
-        String email = request.getEmail();
+    public UserInfo passwordReset(PasswordResetRequest passwordResetRequest) throws MessagingException {
+        String email = passwordResetRequest.getEmail();
 
         UserInfo userInfo = userInfoRepository.findByEmail(email);
 
-        validateUserInfo(userInfo);
-
-        String secureCode = createSecureCode();
-
-        String subject = "Online Quiz Project 비밀번호 재설정 인증";
-
-        MimeMessage mimeMessage = setMailMessage(email, secureCode, subject);
-
-        try{
-
-            javaMailSender.send(mimeMessage);
-
-            List<PasswordAuthCode> authCodes = passwordAuthCodeRepository.findByEmailAndUseYn(email, UseYn.N);
-            if(!authCodes.isEmpty()){
-
-                authCodes.forEach(auth -> {
-                    auth.setUseYn(UseYn.Y);
-                });
-
-                passwordAuthCodeRepository.saveAll(authCodes);
-            }
-
-            PasswordAuthCode passwordAuthCode = PasswordAuthCode.builder()
-                    .authCode(secureCode)
-                    .email(email)
-                    .useYn(UseYn.N)
-                    .authYn(UseYn.N)
-                    .build();
-
-            passwordAuthCodeRepository.save(passwordAuthCode);
-
-            return passwordAuthCode;
-
-        }catch (Exception e){
-
-            throw new CustomException(ErrorCode.EMAIL_SEND_FAILED);
-
-        }
-
-    }
-
-    private void validateUserInfo(UserInfo userInfo) {
         if(userInfo == null){
             throw new CustomException(ErrorCode.USER_NOT_FOUND);
         }
-    }
 
-    public EmailVerifyResponse verify(EmailVerifyRequest request) {
+        String newPassword = createSecureCode();
 
-        PasswordAuthCode passwordAuthCode = passwordAuthCodeRepository
-                .findByEmailAndAuthCodeAndUseYnAndAuthYn(request.getEmail(), request.getAuthCode(), UseYn.N, UseYn.N)
-                .orElseThrow(() -> new CustomException(ErrorCode.AUTH_CODE_OR_EMAIL_NOT_FOUND));
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
-        passwordAuthCode.setUseYn(UseYn.Y);
-        passwordAuthCode.setAuthYn(UseYn.Y);
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
 
-        passwordAuthCodeRepository.save(passwordAuthCode);
+        helper.setTo(email);
 
-        return EmailVerifyResponse.builder()
-                .isVerified(true)
-                .build();
+        helper.setSubject("OQP 임시 비밀번호 발급 이메일");
+
+        String content = "<html>" +
+                "<body style='font-family: Arial, sans-serif; background-color: #f4f7fa; margin: 0; padding: 0;'>" +
+                "<div style='width: 100%; padding: 40px 20px; text-align: center; background-color: #ffffff; border-radius: 15px; box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1); max-width: 600px; margin: 50px auto;'>" +
+
+                "<div style='font-size: 26px; font-weight: bold; color: #333333; margin-bottom: 30px;'>" +
+                "<span style='color: #4CAF50;'>임시 비밀번호 발급</span>" +
+                "</div>" +
+
+                "<p style='font-size: 18px; color: #555555; margin-bottom: 20px;'>아래의 임시 비밀번호를 사용하여 로그인하세요.</p>" +
+
+                "<div style='font-size: 32px; font-weight: bold; color: #ffffff; background-color: #4CAF50; padding: 15px 30px; border-radius: 8px; margin: 20px 0; display: inline-block;'>" +
+                newPassword + "</div>" +
+
+                "<p style='font-size: 16px; color: #555555;'>로그인 후 반드시 비밀번호를 변경해 주세요.</p>" +
+                "<p style='font-size: 16px; color: #555555; margin-bottom: 30px;'>보안을 위해 임시 비밀번호는 타인과 공유하지 마세요.</p>" +
+
+                "<div style='font-size: 14px; color: #888888; margin-top: 40px;'>" +
+                "<p>Online Quiz Project</p>" +
+                "</div>" +
+
+                "</div>" +
+                "</body>" +
+                "</html>";
+
+        helper.setText(content, true);
+
+        try{
+            javaMailSender.send(mimeMessage);
+
+            userInfo.setPassword(passwordEncoder.encode(newPassword));
+
+            return userInfoRepository.save(userInfo);
+        }catch (Exception e){
+            throw new CustomException(ErrorCode.EMAIL_SEND_FAILED);
+        }
     }
 }
